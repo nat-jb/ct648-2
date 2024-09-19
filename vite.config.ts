d98a180925b5f893d3ae0ddc025df25b9045cbff
Natchapol JiB <105231331+nat-jb@users.noreply.github.com>
2024-09-20 00:16:07 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // ตั้งพอร์ตที่ต้องการให้ Vite ใช้
+    host: '0.0.0.0', // เปิดรับการเชื่อมต่อจากเครือข่ายภายนอก
+  },
+
 })
